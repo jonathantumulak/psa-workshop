8b6237ccaeb02bc15e4418229cdc5f789bcb947a
Jonathan Tumulak <tumulak.jonathan@gmail.com>
2017-11-27 17:29:13 +0800

    Allow user to pick username

--- project/project/pipelines.py
+++ project/project/pipelines.py
@@ -1,5 +1,8 @@
 
+from social_core.pipeline.partial import partial
+
 from django.core.urlresolvers import reverse
+from django.shortcuts import render_to_response
 
 
 def send_email_verification(strategy, backend, code, token):
@@ -7,6 +10,17 @@ def send_email_verification(strategy, backend, code, token):
         format(
             reverse('social:complete', args=(backend.name,)), code.code,
             token)
-
     # send email containing verification url
     print url
+
+
+@partial
+def get_username(backend, user=None, *args, **kwargs):
+    if not user:
+        data = backend.strategy.request_data()
+        if data.get('username') is None:
+            return render_to_response('project/pick_username.html')
+        else:
+            print data.get('username')
+            # The user selected a new
+            return {'username': data.get('username')}
